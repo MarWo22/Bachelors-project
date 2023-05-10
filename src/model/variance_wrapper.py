be0bb35f2ed34edf9dc7afb99b87a368b217993a
MarWo22 <83605811+MarWo22@users.noreply.github.com>
2023-05-10 18:07:47 +0200

    Added wrapper for mean node

--- src/model/variance_wrapper.py
+++ src/model/variance_wrapper.py
@@ -16,3 +16,16 @@ class EEGVarianceWrapper(nn.Module):
 
         return variance
     
+class EEGMeanWrapper(nn.Module):
+    def __init__(self, trained_model):
+        super().__init__()
+        self.trained_model = trained_model
+
+    def forward(self, x: torch.Tensor):
+        x = self.trained_model.block1(x)
+        x = self.trained_model.block2(x)
+        x = x.flatten(start_dim=1)
+
+        variance = self.trained_model.mean_node(x)
+
+        return variance
